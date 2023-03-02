1f9432d909461df957fff68b4ec423a2e3375dfd
Bohdan <shbp87@gmail.com>
2023-03-02 14:58:07 +0200

    Initial commit

---
+++ Sportclub/src/main/java/com/Sportsystemsforyou/Sportsystemsforyou/SportsystemsforyouApplication.java
@@ -0,0 +1,13 @@
+package com.Sportsystemsforyou.Sportsystemsforyou;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SportsystemsforyouApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SportsystemsforyouApplication.class, args);
+	}
+
+}
